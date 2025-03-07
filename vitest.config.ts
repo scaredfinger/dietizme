e9e83c3e29ec0a0c87e17ccecb8bc05f7f44a56f
Erich <scaredfinger@gmail.com>
2025-03-07 14:17:34 +0100

    Add Vitest configuration

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+    },
+    // Match Jest's snapshot format
+    snapshotFormat: { 
+      escapeString: true, 
+      printBasicPrototype: true 
+    },
+  },
+});
