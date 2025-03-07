31f08b6977b7f567676ad089a5d90cdb3609be68
Erich <scaredfinger@gmail.com>
2025-03-07 14:23:30 +0100

    Fix Vitest config for utils/graphql

--- libs/utils/graphql/vitest.config.ts
+++ libs/utils/graphql/vitest.config.ts
@@ -2,17 +2,18 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    name: 'utils-logging',
+    name: 'utils-graphql',
     globals: true,
     environment: 'node',
     include: ['**/*.{test,spec}.{ts,js}'],
+    setupFiles: ['./test-setup.ts'],
     typecheck: {
       tsconfig: './tsconfig.spec.json',
     },
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
-      reportsDirectory: '../../build/coverage',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: './build/coverage',
     },
   },
 });
