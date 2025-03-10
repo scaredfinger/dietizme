a6f224c8f84a2078f8f5ecaa12079bcd71a2a8e4
Erich Ledesma <scaredfinger@gmail.com>
2025-03-10 22:01:54 +0000

    chore: missing imports and vitest workspace

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -3,5 +3,6 @@ import { defineWorkspace } from 'vitest/config'
 export default defineWorkspace([
   './libs/utils/logging/vitest.config.ts',
   './libs/utils/common/vitest.config.ts',
+  './libs/utils/graphql/vitest.config.ts',
   './libs/biz-builder/vitest.config.ts',
 ])
